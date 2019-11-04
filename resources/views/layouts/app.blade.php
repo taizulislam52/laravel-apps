ad67a9cb0c0dfb33a2998accea748f1916600956
Taizul52 <islam.taijul133052@gmail.com>
2019-11-04 14:01:55 +0600

    fixed some front-end issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Instagram') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -28,7 +27,7 @@
                         <img src="/svg/logo.svg" alt="" style="height: 20px;border-right: 1px solid black " class="pr-3">
                     </div>
 
-                    <div class="pl-3 pt-1 " >freeCodeGram</div>
+                    <div class="pl-3 pt-1 " >Instagram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,15 +2,49 @@
 
 @section('content')
     <div class="container">
+        @auth
+            <div class="d-flex justify-content-end">
+                <div class="mr-2">
+
+                        <img src="{{auth()->user()->profile->profileImage()}}" alt="" class="rounded-circle" style="height: 50px">
+                </div>
+                <div>
+                    <p class="mb-0">
+                        <a href="/profile/{{auth()->user()->id}}"> {{auth()->user()->username}}</a>
+
+                    </p>
+                    <p class="mt-1">{{auth()->user()->name}}</p>
+                </div>
+            </div>
+            @endauth
         @foreach($posts as $post)
             <div class="row pb-3">
                 <div class="col-5 offset-3">
-                    <a href="/profile/{{$post->user->id}}">
-                        <img src="/storage/{{$post->image}}" alt="" class="w-100">
-                    </a>
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex">
+                            <div class="mr-2">
+                                <img src="/storage/{{$post->user->profile->image}}" alt="" class="mh-100 rounded-circle" style="height: 50px">
+                            </div>
+                            <div>
+                                <p class="mb-0 "><span class="font-weight-bold"><a href="/profile/{{$post->user->id}}"
+                                                                                   style="color: #003569;">{{$post->user->username}}</a></span></p>
+                                <p class="mt-0">{{$post->caption}}</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card-img">
+                            <a href="/profile/{{$post->user->id}}">
+                                <img src="/storage/{{$post->image}}" alt="" class="w-100">
+                            </a>
+                        </div>
+
+                        <div class="card-body"></div>
+                    </div>
                 </div>
             </div>
-            <div class="row pb-3">
+
+             <div class="row pb-3">
                 <div class="col-5 offset-3 bg-white">
                     <p><span class="font-weight-bold"><a href="/profile/{{$post->user->id}}"
                                                          style="color: #003569;">{{$post->user->username}}</a></span> {{$post->caption}}
